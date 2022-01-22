be97c5da5d728dd36b5dfe8741db2c0a770775ca
Karam <karamguliyev10@gmail.com>
2022-01-22 23:11:38 +0300

    Day X - Persisting Cart Info

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -2,6 +2,7 @@ import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import rootReducer from './rootReducer';
+import {persistStore} from 'redux-persist'
 
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './rootSaga';
@@ -13,4 +14,9 @@ export const middlewares = [thunk, logger, sagaMiddleware];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(rootSaga)
 
-export default store;
+export const persistor = persistStore(store)
+
+export default {
+    store,
+    persistor
+};
